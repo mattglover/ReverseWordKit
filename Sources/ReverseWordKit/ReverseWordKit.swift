8b0ac45f033ddf39ca8c46b598b913e966b70477
Matt Glover <matt@duchysoftware.com>
2020-10-09 11:37:59 +0100

    Added public initialiser to ReverseWordKit

--- Sources/ReverseWordKit/ReverseWordKit.swift
+++ Sources/ReverseWordKit/ReverseWordKit.swift
@@ -1,6 +1,9 @@
 import Foundation
 
 public struct ReverseWordKit {
+    
+    public init() {}
+    
     public func reverseWord(_ word: String) -> String {
         let trimmedString = word.trimmingCharacters(in: .whitespacesAndNewlines)
         return String(trimmedString.reversed())
