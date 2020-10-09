540aaeecbf7eedb5b75fdf90a456ed4d287033e7
Matt Glover <matt@duchysoftware.com>
2020-10-09 11:32:12 +0100

    Created implementation of ReverseWordKit

--- Sources/ReverseWordKit/ReverseWordKit.swift
+++ Sources/ReverseWordKit/ReverseWordKit.swift
@@ -1,3 +1,8 @@
-struct ReverseWordKit {
-    var text = "Hello, World!"
+import Foundation
+
+public struct ReverseWordKit {
+    public func reverseWord(_ word: String) -> String {
+        let trimmedString = word.trimmingCharacters(in: .whitespacesAndNewlines)
+        return String(trimmedString.reversed())
+    }
 }
--- Tests/ReverseWordKitTests/ReverseWordKitTests.swift
+++ Tests/ReverseWordKitTests/ReverseWordKitTests.swift
@@ -2,14 +2,17 @@ import XCTest
 @testable import ReverseWordKit
 
 final class ReverseWordKitTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ReverseWordKit().text, "Hello, World!")
+    
+    var sut: ReverseWordKit!
+    
+    func testCanReverseSimpleWord() {
+        sut = ReverseWordKit()
+        let reversed = sut.reverseWord("SimpleWord")
+        
+        XCTAssertEqual(reversed, "droWelpmiS")
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testCanReverseSimpleWord", testCanReverseSimpleWord),
     ]
 }
